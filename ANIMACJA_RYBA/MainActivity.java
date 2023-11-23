7d31e6553e0aebcd28fbe98c50f8af947ea3e539
albim72 <marcin.albiniak72@gmail.com>
2023-11-23 13:23:52 +0100

    Create MainActivity.java

---
+++ ANIMACJA_RYBA/MainActivity.java
@@ -0,0 +1,48 @@
+package com.marcin.animacja_ryba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void obracaj(View v){
+        Animation a = AnimationUtils.loadAnimation(this,R.anim.obrot);
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+        iv.startAnimation(a);
+    }
+
+    public void zanikaj(View v){
+        Animation a = AnimationUtils.loadAnimation(this,R.anim.zanik);
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+        iv.startAnimation(a);
+    }
+
+    public void przesuwaj(View v){
+        Animation a = AnimationUtils.loadAnimation(this,R.anim.przesuw);
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+        iv.startAnimation(a);
+    }
+
+    public void skaluj(View v){
+        Animation a = AnimationUtils.loadAnimation(this,R.anim.skalowanie);
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+        iv.startAnimation(a);
+    }
+
+    public void stosuj_wszystko(View v){
+        Animation a = AnimationUtils.loadAnimation(this,R.anim.wszystko);
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+        iv.startAnimation(a);
+    }
+}
