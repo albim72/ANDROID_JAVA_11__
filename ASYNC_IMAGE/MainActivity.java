235910053bf3db232029be4ccee16b9a8609b38c
albim72 <marcin.albiniak72@gmail.com>
2023-11-23 15:12:07 +0100

    Update MainActivity.java

--- ASYNC_IMAGE/MainActivity.java
+++ ASYNC_IMAGE/MainActivity.java
@@ -1,14 +1,22 @@
 package com.marcin.asyncimage;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,9 +34,69 @@ public class MainActivity extends AppCompatActivity {
         obraz = (ImageView) findViewById(R.id.imageView);
         napis = (TextView) findViewById(R.id.textView);
         stan = (ProgressBar) findViewById(R.id.progressBar);
-        
+
         Bitmap temp = BitmapFactory.decodeResource(getResources(),R.drawable.gory);
-        
+
         bmp = temp.copy(Bitmap.Config.ARGB_8888,true);
+    }
+
+    public void kliknieto_start(View v){
+        Proces p = new Proces();
+        p.execute();
+    }
+    class Proces extends AsyncTask<Void,Void,Void> {
+        private int nr,ile=5000;
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            int szer=bmp.getWidth(),wys=bmp.getHeight();
+            int x,y,kolor;
+            int d=20;
+
+            Canvas c = new Canvas(bmp);
+            Paint p = new Paint();
+            p.setStyle(Paint.Style.STROKE);
+            Random rand = new Random();
+
+            for(nr=0;nr<ile;nr++){
+                x = rand.nextInt(szer);
+                y = rand.nextInt(wys);
+                kolor = bmp.getPixel(x,y);
+
+                p.setColor(kolor);
+                c.drawLine(x-d,y-d,x+d,y+d,p);
+                c.drawLine(x-d,y+d,x+d,y-d,p);
+                publishProgress();
+            }
+            p.setColor(Color.WHITE);
+            c.drawRect(0,0,szer-1,wys-1,p);
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            stan.setVisibility(ProgressBar.VISIBLE);
+            przycisk.setEnabled(false);
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            
+            obraz.setImageBitmap(bmp);
+            przycisk.setEnabled(true);
+            napis.setText("koniec przetwarzania obrazu...");
+            stan.setVisibility(ProgressBar.INVISIBLE);
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+            napis.setText("Stan: " + nr + "/" + ile);
+            if(nr%1000==0)
+                obraz.setImageBitmap(bmp);
+        }
     }
 }
