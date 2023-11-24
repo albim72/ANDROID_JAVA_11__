9c135ba13e33c6693a2a511c1aa39c6a7708459b
albim72 <marcin.albiniak72@gmail.com>
2023-11-24 13:30:43 +0100

    Update DatabaseHandler.java

--- SQLITE_CONTACT/DatabaseHandler.java
+++ SQLITE_CONTACT/DatabaseHandler.java
@@ -1,1 +1,40 @@
 
+package com.marcin.sqlite_kontakt;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import kotlin.text.UStringsKt;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "contactsManager";
+    private static final String TABLE_NAME = "contacts";
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_PH_NO = "phone_number";
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
+                KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + 
+                KEY_NAME + " TEXT, " + KEY_PH_NO + " TEXT);";
+
+        db.execSQL(CREATE_CONTACTS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME  +";");
+        onCreate(db);
+    }
+    
+}
