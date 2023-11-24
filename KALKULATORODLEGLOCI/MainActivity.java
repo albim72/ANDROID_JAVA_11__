ee466cf7312aca14ed0e4ec375b6b079b84ae021
albim72 <marcin.albiniak72@gmail.com>
2023-11-24 09:42:33 +0100

    Update MainActivity.java

--- KALKULATORODLEGLOCI/MainActivity.java
+++ KALKULATORODLEGLOCI/MainActivity.java
@@ -3,6 +3,7 @@ package com.marcin.kalulatorodleglosci;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -43,5 +44,50 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        
+        milaM = (RadioButton) findViewById(R.id.rbmilaM);
+        milaL = (RadioButton) findViewById(R.id.rbmilaL);
+        jard = (RadioButton) findViewById(R.id.rbjard);
+        stopa = (RadioButton) findViewById(R.id.rbstopa);
+        cal = (RadioButton) findViewById(R.id.rbcal);
+        textView = (TextView) findViewById(R.id.wynik);
+        
+        button = (Button) findViewById(R.id.button);
+        dist = (EditText) findViewById(R.id.etMetry);
+        
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                final double ds = new Double(dist.getText().toString());
+                
+                java.text.DecimalFormat df = new java.text.DecimalFormat();
+                df.setMaximumFractionDigits(2);
+                df.setMinimumFractionDigits(2);
+                
+                if (selectedId == milaM.getId()){
+                    Anglo mm = new Anglo(ds,IPrzeliczniki.milaPP);
+                    textView.setText("odległość w milach morskich: " + df.format(mm.policzOdl()));
+                }
+                else if (selectedId == milaL.getId()){
+                    Anglo ml = new Anglo(ds,IPrzeliczniki.milaLP);
+                    textView.setText("odległość w milach lądowych: " + df.format(ml.policzOdl()));
+                }
+                else if (selectedId == jard.getId()){
+                    Anglo jd = new Anglo(ds,IPrzeliczniki.jardP);
+                    textView.setText("odległość w jardach: " + df.format(jd.policzOdl()));
+                }
+                else if (selectedId == stopa.getId()){
+                    Anglo st = new Anglo(ds,IPrzeliczniki.stopaP);
+                    textView.setText("odległość w stopach: " + df.format(st.policzOdl()));
+                }
+
+                else if (selectedId == cal.getId()){
+                    Anglo cl = new Anglo(ds,IPrzeliczniki.calP);
+                    textView.setText("odległość w calach: " + df.format(cl.policzOdl()));
+                }
+            }
+        });
+        
     }
 }
