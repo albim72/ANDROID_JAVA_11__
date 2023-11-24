4be94c58eb8dd3d4c79c3b58d8453f00f180aafb
albim72 <marcin.albiniak72@gmail.com>
2023-11-24 13:54:19 +0100

    Update DatabaseHandler.java

--- SQLITE_CONTACT/DatabaseHandler.java
+++ SQLITE_CONTACT/DatabaseHandler.java
@@ -1,11 +1,15 @@
-
 package com.marcin.sqlite_kontakt;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import kotlin.text.UStringsKt;
 
@@ -25,7 +29,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
-                KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + 
+                KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 KEY_NAME + " TEXT, " + KEY_PH_NO + " TEXT);";
 
         db.execSQL(CREATE_CONTACTS_TABLE);
@@ -36,5 +40,48 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME  +";");
         onCreate(db);
     }
+
+    void addContact(Contact contact){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(KEY_NAME, contact.getName());
+        values.put(KEY_PH_NO,contact.getPhone());
+        db.insert(TABLE_NAME,null,values);
+        db.close();
+    }
+
+
+    Contact getContact(int id){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_NAME,new String[]{KEY_ID,KEY_NAME,KEY_PH_NO},
+                KEY_ID + "=",new String[]{String.valueOf(id)},null,null,null,null
+                );
+        if(cursor!=null){
+            cursor.moveToFirst();
+            Contact contact = new Contact(Integer.parseInt(cursor.getString(0)), cursor.getString(1),
+                    cursor.getString(2));
+            return contact;
+        }
+        return null;
+    }
     
+    public List<Contact> getAllContacts() {
+        List<Contact> contactList = new ArrayList<>();
+        String selectQuery = "SELECT * FROM " + TABLE_NAME + ";";
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToNext()) {
+            do {
+                Contact contact = new Contact();
+                contact.setId(Integer.parseInt(cursor.getString(0)));
+                contact.setName(cursor.getString(1));
+                contact.setPhone(cursor.getString(2));
+                contactList.add(contact);
+
+            }
+            while (cursor.moveToNext());
+        }
+        return contactList;
+    }
 }
